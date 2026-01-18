6011e33f6e3802637915ec0c8ae1d2481d80bc3e
wallscreet <wallscreet@gmail.com>
2026-01-18 17:28:52 -0600

    projects github icon size

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -30,16 +30,8 @@ export default function ProjectPage() {
               {project.description && (
                 <p className="text-white mb-3">{project.description}</p>
               )}
+              
               <div className="grid grid-cols-3">
-                {/* <div className="text-sm col-span-2">
-                  <span className="text-green-300">$ cat </span>
-                  <Link
-                    href={`/projects/${project.slug}`}
-                    className="text-blue-500 hover:text-green-300"
-                  >
-                    ./projects/{project.slug}.md
-                  </Link>
-                </div> */}
                 <div className="col-span-2">
                   <span className="text-sm">({project.language})</span>
                 </div>
@@ -56,7 +48,7 @@ export default function ProjectPage() {
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      className="w-5 h-5"
+                      className="w-7 h-7"
                     >
                       <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.207 11.385.6.113.793-.258.793-.577 0-.285-.01-1.04-.016-2.04-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.236 1.84 1.236 1.07 1.834 2.807 1.304 3.492.997.108-.776.418-1.305.76-1.605-2.665-.303-5.466-1.332-5.466-5.93 0-1.31.468-2.38 1.236-3.22-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.5 11.5 0 0 1 3.003-.404c1.02.005 2.047.138 3.003.404 2.29-1.552 3.296-1.23 3.296-1.23.655 1.653.243 2.873.12 3.176.77.84 1.234 1.91 1.234 3.22 0 4.61-2.807 5.624-5.48 5.92.43.372.814 1.102.814 2.222 0 1.604-.015 2.896-.015 3.286 0 .322.192.694.8.576C20.565 21.796 24 17.298 24 12 24 5.37 18.63 0 12 0z" />
                     </svg>
@@ -68,7 +60,7 @@ export default function ProjectPage() {
           ))}
         </div>
 
-        <div className="mt-12 border border-white rounded p-6">
+        <div className="mt-8 border border-white rounded p-6">
           <h2 className="text-xl mb-4">$ ./stats.sh</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 text-sm">
             <div>
@@ -86,7 +78,7 @@ export default function ProjectPage() {
           </div>
         </div>
 
-        <div className="text-center mt-8">
+        <div className="text-center mt-6">
           <a href="/" className="inline-block border text-blue-500 border-blue-500 rounded px-6 py-3 hover:bg-green-950 transition-colors">
             $ cd /home
           </a>
